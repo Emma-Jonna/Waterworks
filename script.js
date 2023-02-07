c433619ec1f6d93b28a93c05ffb6fbebc7021a0b
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-02-07 15:42:32 +0100

    updated script

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const getInfo = async () => {
   const response = await fetch(
-    "https://data.goteborg.se/RiverService/v1.1/MeasureSites/api-key/Nedsjon?&format=Json"
+    "https://data.goteborg.se/RiverService/v1.1/MeasureSites/api-key?&format=Json"
   );
 
   const resp = await response.json();
 
-  console.log(resp);
+  console.log(resp[0].Description);
+
+  resp.forEach((element) => {
+    console.log(element.Description);
+  });
 
   try {
   } catch (error) {}
