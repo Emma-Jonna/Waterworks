9c261203baef7ca78f4b233e9e89d33f60e46a2b
Emma-Jonna <johanna.emma.pihl@gmail.com>
2023-02-07 15:01:02 +0100

    updated gitignore

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const getInfo = async () => {
   const response = await fetch(
-    "https://data.goteborg.se/RiverService/v1.1/MeasureSites/f43ea846-245a-4797-b66b-d34b33e98df6/Nedsjon?&format=Json"
+    "https://data.goteborg.se/RiverService/v1.1/MeasureSites/api-key/Nedsjon?&format=Json"
   );
 
   const resp = await response.json();
